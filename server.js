f6410ce7e50987b6bdf45304b8b7b8a3afc116d3
mgouault <gouault.matthieu@gmail.com>
2016-05-06 17:01:16 +0200

    save

--- server.js
+++ server.js
@@ -2,47 +2,42 @@ var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var rp = require('request-promise');
+var url = require('url');
+var config = require('config');
+var _ = require('lodash');
+
 var app = express();
-
-var baseURL = 'http://widukind-api-dev.cepremap.org/api/v1/json';
-app.set('port', (process.env.PORT || 3000));
-
+app.set('port', (process.env.PORT || config.get('app.port')));
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-// Additional middleware which will set headers that we need on each request.
 app.use(function(req, res, next) {
-    // Set permissive CORS header - this allows this server to be used only as
-    // an API server in conjunction with something like webpack-dev-server.
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Disable caching so we'll always get the latest comments.
-    res.setHeader('Cache-Control', 'no-cache');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
 });
 
+
+
 app.use('/data/:key', function (req, res, next) {
-  var pathname;
+  var URLObj = config.get('api.URLObj');
   switch (req.params.key) {
     case 'providers':
-      pathname = '/providers/keys'; break;
+      URLObj.pathname += '/providers/keys'; break;
     case 'datasets':
-      pathname = '/providers/'+req.query.provider+'/datasets/keys'; break;
+      URLObj.pathname += '/providers/'+req.query.provider+'/datasets/keys'; break;
     case 'dimensions':
-      pathname = '/datasets/'+req.query.dataset+'/dimensions'; break;
+      URLObj.pathname += '/datasets/'+req.query.dataset+'/dimensions'; break;
     case 'json':
-      pathname = '/datasets/'+req.query.dataset+'/values'
-        + '?limit=10'
-        + req.query.querystring;
+      URLObj.pathname += '/datasets/'+req.query.dataset+'/values';
+      _.assign(URLObj.query, req.query.controls);
       break;
+    default:
+      var err = new Error();
+      err.status = 400;
+      return next(err);
   }
-  if (!pathname) {
-    var err = new Error();
-    err.status = 400;
-    return next(err);
-  }
-  rp(baseURL + pathname)
+  rp(url.format(URLObj))
     .then(function (response) {
       req.responseData = response;
       next();
@@ -55,6 +50,8 @@ app.use('/data/:key', function (req, res, next) {
 app.use(function (err, req, res, next) {
   res.status(err.status).json(err);
 });
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:'+app.get('port')+'/');
