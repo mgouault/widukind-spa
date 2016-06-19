e3c4491cac2a3bd3cacdd62df1ca063a277f2c2b
mgouault <mgouault@student.42.fr>
2016-06-19 21:58:55 +0200

    navbar modified

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, Nav, Navbar, NavItem } from 'react-bootstrap';
 
 var ParamsContainer = require('./params/Container.jsx');
 var ResponseContainer = require('./response/Container.jsx');
@@ -14,16 +14,24 @@ var WidukindSPA = React.createClass({
     return (
       <Grid fluid>
 
-        <div className="header clearfix">
-          <nav>
-            <ul className="nav nav-pills pull-right">
-              <li role="presentation"><a href="//github.com/mgouault/widukind-spa" target="_blank"><img src="assets/github.png" alt="repo github" /></a></li>
-              <li role="presentation"><a href="//widukind.cepremap.org" target="_blank">Widukind</a></li>
-              <li role="presentation"><a href="//widukind-api.cepremap.org" target="_blank">API</a></li>
-            </ul>
-          </nav>
-          <h3 className="text-muted">Widukind SPA</h3>
-        </div>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              Widukind SPA
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="//github.com/mgouault/widukind-spa" target="_blank">
+              <img src="assets/github.png" alt="repo github" />
+            </NavItem>
+            <NavItem eventKey={2} href="//widukind.cepremap.org" target="_blank">
+              Widukind
+            </NavItem>
+            <NavItem eventKey={3} href="//widukind-api.cepremap.org" target="_blank">
+              API
+            </NavItem>
+          </Nav>
+        </Navbar>
 
         <div className="row marketing">
           <Row>
